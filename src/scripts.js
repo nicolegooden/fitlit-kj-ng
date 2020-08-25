d9dc4cba560e3b7b56d9c568d7f1aa573f7ada3c
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-08-25 13:54:19 -0600

    Make createUser() to instantiate new User on load

--- src/scripts.js
+++ src/scripts.js
@@ -3,31 +3,42 @@
 let greeting = document.querySelector('.greeting');
 let stepGoalComparison = document.querySelector('.you-vs-others-data');
 
+
 let userRepository = new UserRepository(userData);
+let user = createUser();
 
 ///// event listeners /////
 
 window.addEventListener('load', compareStepGoals);
+window.addEventListener('load', createUser);
+window.addEventListener('load', welcomeUser);
 
 ///// event handlers /////
 
 function welcomeUser() {
-  let newRandomUser = getRandomUser();
-  let user = new User(newRandomUser);
   let userFirstName = user.getFirstName();
   greeting.innerText = `Welcome, ${userFirstName}!`;
   return user;
 };
 
-function getRandomUser() {
+function getRandomID() {
   let randomUserID = Math.floor(Math.random() * userRepository.allUserData.length);
   let randomUser = userRepository.getUserData(randomUserID);
   return randomUser;
 };
 
 function compareStepGoals() {
-  let userForStepGoals = welcomeUser();
   let averageStepGoal = userRepository.calculateAverageSteps();
-  let userStepGoal = userForStepGoals.dailyStepGoal;
+  let userStepGoal = user.dailyStepGoal;
   stepGoalComparison.innerText = `Your step goal: ${userStepGoal}\n Average User\'s Step Goal: ${averageStepGoal}`;
 };
+
+function createUser() {
+  let newRandomUser = getRandomID();
+  let user = new User(newRandomUser);
+  return user;
+};
+
+function showUserCardData() {
+
+}
