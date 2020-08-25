6a98a98af29f2cfa45c7bdd22fa492b10355e31e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-24 18:47:58 -0600

    Add welcomeUser() with helper function to greet user

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+////// query selectors //////
+
+let greeting = document.querySelector('.greeting');
+
+///// event listeners /////
+
+window.addEventListener('load', welcomeUser);
+
+///// event handlers /////
+
+function welcomeUser() {
+  let newRandomUser = getRandomUser();
+  let user = new User(newRandomUser);
+  let userFirstName = user.getFirstName();
+  greeting.innerText = `Welcome, ${userFirstName}!`;
+};
+
+function getRandomUser() {
+  let userRepository = new UserRepository(userData);
+  let randomUserID = Math.floor(Math.random() * userRepository.allUserData.length);
+  let randomUser = userRepository.getUserData(randomUserID);
+  return randomUser;
+};
